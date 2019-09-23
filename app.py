bad28fe94ecbf85c0bbaf7a559aef3e4ec69b86f
Abdulmateen Tairu <tay2druh@gmail.com>
2019-09-23 01:20:47 +0100

    CRUD features

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from chalice import Chalice
 from pymongo import MongoClient
 import json
 from bson import ObjectId
+
+
 client = MongoClient('mongodb+srv://tay2druh:olawale.@tay-canvy.mongodb.net/test?retryWrites=true&w=majority')
 db = client.get_database('products_db')
 table = db.products_records
@@ -19,12 +21,12 @@ app = Chalice(app_name='tayshop')
 
 @app.route('/')
 def index():
-    return JSONEncoder().encode(list(table.find())[0])
+    return JSONEncoder().encode(list(table.find()))
 
 
 @app.route('/all', methods=['GET'])
 def all_products():
-    return JSONEncoder().encode(list(table.find())[0])
+    return JSONEncoder().encode(list(table.find()))
 
 
 @app.route('/{name}')
@@ -33,22 +35,29 @@ def product(name):
     return JSONEncoder().encode(data)
 
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.route('/create', methods=['POST'])
+def create_product():
+    request = app.current_request
+    body = request.json_body
+    table.insert_one(body)
+    return {'message': 'Successful addition'}
+
+
+@app.route('/{name}/delete', methods=['POST'])
+def product_delete(name):
+    data = table.find_one({'name': name})
+    if data is not None:
+        table.delete_one(data)
+        return {'message': 'Successful Deleting'}
+    else:
+        return {'message': 'Product not Found'}
+
+
+@app.route('/{name}/update', methods=['POST'])
+def product_update(name):
+    request = app.current_request
+    data = table.find_one({'name': name})
+    body = request.json_body
+    new_data = {'$set': body}
+    table.update_one(data, new_data)
+    return {'message': 'Successful Updating'}
